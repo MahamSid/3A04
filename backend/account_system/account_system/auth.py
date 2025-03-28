30ac69cc29770677a7eeb4cf9210033a6ba1a43f
MahamSid <77751674+MahamSid@users.noreply.github.com>
2025-03-28 17:29:00 -0400

    modified login

--- backend/account_system/account_system/auth.py
+++ backend/account_system/account_system/auth.py
@@ -1,6 +1,23 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+#Openpyxl: used to access excel file 
+from openpyxl import load_workbook
+#import os
+
 
 class Account(ABC):
-    @abstractmethod
-    def authenticate(self, loginID: str, password: str) -> bool:
-        pass
+
+    def __init__(self, loginID, password):
+        self.loginID = loginID
+        self.password = password
+    #    self.dbfilepath = None
+    #    self.accdb = load_workbook("Account Database.xlsx")
+'''
+ #double check error handling   
+    def set_dbfilepath(self, dbfilepath):
+        self.dbfilepath = dbfilepath
+'''
+
+if __name__ == '__main__':
+    acc = Account('log1', 'pw')
+    print(acc.loginID)
+    print(acc.password)
