7a4fdc27f54d47da04de099512efaa0e2eb3e3c4
zayedu <umerzayed1@gmail.com>
2025-03-31 15:19:33 -0400

    feat: add upload image

--- backend/UltrasoundAnalyzer/app.py
+++ backend/UltrasoundAnalyzer/app.py
@@ -7,29 +7,29 @@ from predict import predict_img
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/ultrasound_analyzer/predict', methods=['POST'])
+@app.route('/ultrasound_analyzer/predict', methods=['GET'])
 def predict_pcos():
-    # Check if the request contains the 'image' file
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image file in request'}), 400
-
-    file = request.files['image']
-    if file.filename == '':
-        return jsonify({'error': 'Empty filename'}), 400
-
-    # Save the uploaded file to a temporary directory
-    temp_dir = tempfile.gettempdir()
-    temp_path = os.path.join(temp_dir, file.filename)
-    file.save(temp_path)
-
+    temp_path = 'last_upload_img.jpg'
     try:
         # Call predict_img (defined in predict.py) to get the PCOS likelihood percentage
         pcos_likelihood_percentage = predict_img(temp_path)
-        # Remove the temporary file after prediction
-        os.remove(temp_path)
         return jsonify({'PCOS Likelihood': f"{pcos_likelihood_percentage:.2f}%"})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/ultrasound_analyzer/uploadImage', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return 'No file part'
+    file = request.files['image']
+    #save in a temp directory with name last_upload_img.jpg
+    file.save('last_upload_img.jpg')
+    return jsonify(200,{'success': 'Image uploaded successfully'})
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#TODO: Save image locally and have predict look at that image
+
+#80% of course evals for 3s03
